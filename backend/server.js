86479ac78ade6498389fa5f0b2beae36c98b9d69
Setthawut Pimthong <oop_best@yahoo.com>
2025-03-22 15:44:52 +0700

    deployment

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.route.js";
@@ -10,6 +11,7 @@ import { app, server } from "./socket/socket.js";
 dotenv.config();
 
 const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
 
 app.use(express.json()); // for parsing incoming requests with JSON payloads
 app.use(cookieParser());
@@ -17,6 +19,11 @@ app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", usersRoutes);
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "/frontend", "dist", "index.html"));
+});
 
 server.listen(PORT, () => {
   connectDB();
